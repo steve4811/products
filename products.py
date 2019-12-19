5f9ede828d46dc66fab641f165fafda905e01ba8
Steve Hsiao <steve550610@hotmail.com>
2019-12-19 17:27:28 +0800

    add products

---
+++ products.py
@@ -0,0 +1,11 @@
+products =[]
+
+while True:
+	product_name = input('請輸入你買過的商品 ：')
+	if product_name == 'q':
+		break
+	else:
+		product_price = input('請輸入商品價格 ：')
+		products.append([product_name ,product_price])
+
+print(products)
